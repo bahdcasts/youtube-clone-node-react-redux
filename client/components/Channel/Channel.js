6d4ae9586ea37e7cba940844cb6b461506bb098a
Kati Frantz <bahdcoder@gmail.com>
2019-03-11 10:17:59 +0100

    lesson-27-add-update-channel-details-form

--- client/components/Channel/Channel.js
+++ client/components/Channel/Channel.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Formik } from 'formik'
 import PropTypes from 'prop-types'
 
 // components
@@ -13,7 +14,47 @@ const Channel = ({ channel }) => (
         {channel.isFetching ? (
           <p>loading ...</p>
         ) : (
-          <div>{channel.channel.name}</div>
+          <Formik
+            onSubmit={console.log}
+            initialValues={{
+              name: channel.channel.name,
+              description: channel.channel.description || ''
+            }}
+          >
+            {({ values, handleChange, handleBlur, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    id="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="description">Description</label>
+                  <textarea
+                    name="description"
+                    id="description"
+                    cols="3"
+                    rows="3"
+                    className="form-control"
+                    value={values.description}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-success">
+                  Update
+                </button>
+              </form>
+            )}
+          </Formik>
         )}
       </div>
     </div>
